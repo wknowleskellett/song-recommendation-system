63f0117e7ce9e9259a8829348e07c230bb5990c6
Osama Abdelrahman <odawood@oberlin.edu>
2022-01-04 21:44:58 -0500

    now the program gets the features of the user's top tracks

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import spotipy
 from spotipy.oauth2 import SpotifyOAuth
+from pprint import pprint
 
 cid = '5afdc63179404ae299ca0fe6ea3f42f6'
 secret = '150228edfc42438fb2e627ac0127b412'
@@ -10,7 +11,15 @@ birdy_uri = 'spotify:artist:2WX2uTcsvV5OnS0inACecP'
 #user sign in
 sp = spotipy.Spotify(auth_manager=SpotifyOAuth(scope=scope, client_id=cid, client_secret=secret, redirect_uri=uri))
 
-#get the users top 5 songs
-results = sp.current_user_top_tracks()
-for i, item in enumerate(results['items'][:5]):
-   print(i+1, ") track: ", item['name'], ", artist: ", item['artists'][0]['name'], ", track ID: ", item['id'])
+#get the users top 5 songs 
+track_id = [] #list that stores the track ids of the user's top tracks
+results = sp.current_user_top_tracks() 
+for i, item in enumerate(results['items'][:5]): 
+   print(i+1, ") track: ", item['name'], ", artist: ", item['artists'][0]['name'], ", track ID: ", item['id']) 
+   track_id.append(item['id'])
+
+#get audio features for a series of tracks
+results = sp.audio_features(track_id) #results has a list of dictionaries, which contain each song's features
+pprint(results)
+
+
